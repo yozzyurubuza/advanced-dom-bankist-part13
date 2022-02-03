9d10e3b05cbb67db0c91572a3eec84c09d8bcfed
yozzyurubuza <matt_ebarle15@yahoo.com>
2022-02-04 01:56:10 +0800

    Added codes about Event Delegation: Implementing Page Navigation

---
+++ 06-eventDelegation.js
@@ -0,0 +1,68 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+///////////////////////////////////////
+// Modal window
+
+const openModal = function (e) {
+  e.preventDefault();
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
+
+btnScrollTo.addEventListener('click', function (e) {
+  //New implementation
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+// Event Delegation: Implementing Page Navigation
+
+// Page Navigation
+// document.querySelectorAll('.nav__link').forEach(function (el) {
+//   el.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute('href');
+//     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+//   });
+// });
+
+// Event Delegation
+
+// 1. Add event listener to common parent element
+// 2. Determine what element originated the event
+
+//Parent
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+  //   console.log(e.target); // Identify where the event happened
+
+  // Matching strategy
+  // Event delegation - attaching only to the class with the same class without using loop.
+  //Faster than implementing a loop
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
